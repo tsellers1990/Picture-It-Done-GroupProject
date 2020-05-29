e5cf7cca8b8b0b1793b4aa4aebe8c94cabb33782
Zach Proctor <z.proctor@hotmail.com>
2020-05-29 14:24:32 -0600

    adding .json components and css page

--- client/src/App.js
+++ client/src/App.js
@@ -1,2 +1,27 @@
-import React from "react";
-//use some other stuff too
+import React from 'react';
+import './App.css';
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Contact from "./pages/Contact/Contact";
+
+class App extends React.Component {
+  state={name: "Picture It Done",
+  tab1: "Portfolio"
+  }
+
+
+  render(){
+    return (
+      <Router>
+        <Navbar name={this.state.name} portfolio={this.state.tab1}/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/Contact" component={Contact}/>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
