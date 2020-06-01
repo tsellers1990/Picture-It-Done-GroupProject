f25fef782858bf232dad287ed3eb58fa1a229885
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-01 12:16:14 -0600

    Set up the api routes in the clint utils folder. added getJobs, getJob, deleteJob, saveJob

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 export default {
-    //have the routes here
-
-    //getJobs
-
-    //getJob
-
-    //deleteJob
-}
+    getJobs: function() {
+        return axios.get("/api/jobs");
+    },
+    getJob: function(id) {
+        return axios.get("/api/jobs" + id);
+    },
+    deleteJob: function(id) {
+        return axios.delete("/api/jobs" + id);     
+    },
+    saveJob: function(jobData) {
+        return axios.post("/api/jobs" + jobData);
+    }
+};
