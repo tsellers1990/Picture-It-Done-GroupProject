5fa21c5e03e8cac8d912efbe87c49f6db28246dd
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-02 11:06:30 -0600

    built out a wrapper.js component

--- client/src/components/Add Job/AddJob.js
+++ client/src/components/Add Job/AddJob.js
@@ -3,4 +3,6 @@ import { Component } from "materialize-css";
 // any other imports, You might need a few such as the functional component for the db call
 
 //choosing to use a class component because that's what I did in hw 19 and it works.
-const DataTable({ })
+const DataTable() {
+
+}
