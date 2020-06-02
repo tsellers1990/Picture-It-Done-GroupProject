f82e8e74db2736eb28864274a5618608ec9e3359
Zach Proctor <z.proctor@hotmail.com>
2020-06-02 15:42:57 -0600

    further changes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 
+
 class Home extends React.Component {
   render(){
     return (
       <>
+       
         <h1>Welcome to the Site!</h1>
         <div id="container">
         <p>The goal of Picture It Done is quite simple. Our missing is to connect you with the best person for your weekend job or home remodel! We have built up certified partners in nearly every avenue of home improvement, plumbing, gardening and landscaping, or anything else you can dream up.</p>
