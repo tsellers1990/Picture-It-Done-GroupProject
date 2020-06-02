af9dd9c9b91d211d44f56c9d9ff1293ca319802d
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-02 12:13:29 -0600

    built part of the form, still have to polish but the Form components are built and being imported!

--- client/src/components/Add Job/AddJob.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Component } from "materialize-css";
-// any other imports, You might need a few such as the functional component for the db call
-
-//choosing to use a class component because that's what I did in hw 19 and it works.
-const DataTable() {
-
-}
--- client/src/pages/AddJob/AddJob.js
+++ client/src/pages/AddJob/AddJob.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import { Input, TextArea, FormBtn, UploadBtn } from '../../components/Form/index'
 
 class AddJobs extends React.Component {
+
+  // function handleInputChange(event) {
+  //   const { name, value } = event.target;
+  //   setFormObject({...formObject, [name]: value})
+  // };
+
   render(){
     return (
-      <h1>Post a job</h1>
+      <div className="new-job-form">
+        <form>
+          <Input 
+            // onChange={handleInputChange}
+            name="title"
+            placeholder="Your Post Title (required)"
+            style={{ marginTop: 10}}
+          />
+          <Input 
+            // onChange={handleInputChange}
+            name="title"
+            placeholder="Please describe your problem"
+            style={{ marginTop: 10}}
+          />
+          <UploadBtn/>
+          <FormBtn />
+        </form>
+      </div>
     );
   }
 }
