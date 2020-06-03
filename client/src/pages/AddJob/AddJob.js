48e217c389e9b5c3ef9465f86e991ca9b45a4665
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-03 11:33:02 -0600

    added phone and email to the handleformsubmit and the jobschema

--- client/src/pages/AddJob/AddJob.js
+++ client/src/pages/AddJob/AddJob.js
@@ -21,7 +21,9 @@ const AddJobs = () => {
         user: formObject.user,
         description: formObject.description,
         date: Date.now,
-        image: formObject.image
+        image: formObject.image,
+        phone: formObject.phone,
+        email: formObject.email
       })
       .catch(err => console.log(err));
     }
@@ -41,6 +43,18 @@ const AddJobs = () => {
           onChange={handleInputChange}
           name="description"
           placeholder="Please Describe Your Problem"
+          style={{ marginTop: 10}}
+        />
+        <Input 
+          onChange={handleInputChange}
+          name="phone"
+          placeholder="Phone Number (optional)"
+          style={{ marginTop: 10}}
+        />
+        <Input 
+          onChange={handleInputChange}
+          name="email"
+          placeholder="Email (optional)"
           style={{ marginTop: 10}}
         />
         <UploadBtn /> {/* add logic here to handle the upload functionality */}
