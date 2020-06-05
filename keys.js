cec612a05eda5bbd1a6ce3b614b4b5f7b8321041
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-05 07:33:32 -0600

    hiding api keys

--- keys.js
+++
@@ -1,7 +0,0 @@
-const keys = {
-    s3bucket: "bucket-for-picture-it-done-image-files",
-    s3key: "AKIAS5OCRH6NLLQLDV4V",
-    s3secret: "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"
-};
-
-module.exports = keys;
--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(fileUpload());
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   // app.use(express.static("client/"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/"));
+}
 // Add routes, both API and view
 app.use(routes);
 // app.use('/api/', fileRoutes);
